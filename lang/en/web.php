711fc2df3a516b4bd4be23dac043cf23e7d36abe
ronniefaith <ronnie.paralejas@gmail.com>
2024-05-18 12:03:29 +0700

    Translation

--- lang/en/web.php
+++ lang/en/web.php
@@ -3,4 +3,6 @@
 return [
     'month' => 'Month',
     'announcements' => 'Announcements',
+    'show_more_posts' => 'Show more posts',
+    'news_events' => 'News & Events',
 ];
--- lang/vi/web.php
+++ lang/vi/web.php
@@ -3,4 +3,6 @@
 return [
     'month' => 'Tháng',
     'announcements' => 'Thông báo',
+    'show_more_posts' => 'Xem thêm bài viết',
+    'news_events' => 'Tin tức & Sự kiện',
 ];
