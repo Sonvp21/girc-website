3e42042319074e667081f1e3dd08994048c73f6d
ronniefaith <ronnie.paralejas@gmail.com>
2024-05-18 12:03:29 +0700

    Pint blade

--- resources/views/components/website/home-post.blade.php
+++ resources/views/components/website/home-post.blade.php
@@ -6,24 +6,28 @@
         >
             <article>
                 <figure>
-                    <div class="bg-red-500 relative">
-                        <img class="w-full h-auto" src="{{ $latestPost->getFirstMedia('featured_image')->getUrl('lg') }}" alt="">
-                        <div class="text-xs flex w-fit items-center gap-2 text-white p-2 bg-green-700 absolute right-0 bottom-0">
-                            <x-heroicon-m-calendar class="size-4"/>
+                    <div class="relative bg-red-500">
+                        <img
+                            class="h-auto w-full"
+                            src="{{ $latestPost->getFirstMedia('featured_image')->getUrl('lg') }}"
+                            alt=""
+                        />
+                        <div class="absolute bottom-0 right-0 flex w-fit items-center gap-2 bg-green-700 p-2 text-xs text-white">
+                            <x-heroicon-m-calendar class="size-4" />
                             <span>{{ $latestPost->published_post_date }}</span>
                         </div>
                     </div>
-                    <h2 class="py-2 text-justify font-roboto text-xl font-extrabold tracking-tight text-green-700 group-hover:text-blue-800 line-clamp-2 h-20">
+                    <h2 class="line-clamp-2 h-20 py-2 text-justify font-roboto text-xl font-extrabold tracking-tight text-green-700 group-hover:text-blue-800">
                         {{ $latestPost->title }}
                     </h2>
                 </figure>
 
-                <p class="text-justify font-roboto font-normal leading-5 text-slate-500 line-clamp-6">
+                <p class="line-clamp-6 text-justify font-roboto font-normal leading-5 text-slate-500">
                     {{ Str::limit(html_entity_decode(strip_tags($latestPost->content)), 500) }}
                 </p>
             </article>
         </a>
-        <div class="col-span-5 md:col-span-2 flex flex-col justify-between">
+        <div class="col-span-5 flex flex-col justify-between md:col-span-2">
             <div class="flex items-center gap-2 border-x-4 border-green-700 bg-white px-4 py-3 font-semibold uppercase text-green-700">
                 <x-heroicon-o-newspaper class="size-5" />
                 <span>@lang('web.news_events')</span>
@@ -51,14 +55,18 @@
                                         <a
                                             href=""
                                             class="hover:text-rose-600 line-clamp-3 leading-5"
-                                        >{{ $post->title }}</a>
-                                        <div class="text-green-700 flex justify-between gap-2 pt-2 text-xs">
+                                            >{{ $post->title }}</a
+                                        >
+                                        <div class="flex justify-between gap-2 pt-2 text-xs text-green-700">
                                             <a
                                                 href="#"
                                                 class="text-xs hover:underline"
-                                            >{{ $post->category->title }}</a
+                                                >{{ $post->category->title }}</a
                                             >
-                                            <div class="flex items-center gap-2 tooltip tooltip-left "  data-tip="{{ $post->published_post_date }}">
+                                            <div
+                                                class="tooltip tooltip-left flex items-center gap-2"
+                                                data-tip="{{ $post->published_post_date }}"
+                                            >
                                                 <span class="text-xs">{{ $post->published_post_date_thumb }}</span>
                                             </div>
                                         </div>
@@ -69,8 +77,12 @@
                     </div>
                 @endforeach
             </div>
-            <div class="mt-4 text-center w-full">
-                <a class="flex justify-center w-full border-green-800 bg-green-700 text-white py-3 text-sm font-bold hover:shadow-lg hover:bg-green-800" href="#">@lang('web.show_more_posts')</a>
+            <div class="mt-4 w-full text-center">
+                <a
+                    class="flex w-full justify-center border-green-800 bg-green-700 py-3 text-sm font-bold text-white hover:bg-green-800 hover:shadow-lg"
+                    href="#"
+                    >@lang('web.show_more_posts')
+                     </a>
             </div>
         </div>
     </div>
