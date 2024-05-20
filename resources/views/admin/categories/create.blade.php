10bd722a3e80b6a77bd87ad3920c05892848d0a7
ronniefaith <ronnie.paralejas@gmail.com>
2024-05-20 22:02:52 +0700

    Update album categories

--- resources/views/admin/categories/create.blade.php
+++ resources/views/admin/categories/create.blade.php
@@ -12,32 +12,30 @@
         <div class="mt-6">
             <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg">
                 <div class="bg-white p-4 shadow sm:rounded-lg sm:p-8">
-                    <div class="max-w-xl">
-                        <form action="{{ route('admin.categories.store') }}" method="POST" class="needs-validation"
-                            novalidate>
-                            @csrf
-                            <div class="row mb-3">
-                                <div class="label">
-                                    <span class="label-text">Title</span>
-                                </div>
-                                <input type="text" name="title" placeholder="Type here"
-                                    @class([
-                                        'input',
-                                        'input-bordered',
-                                        'input-error' => $errors->has('title'),
-                                        'w-full',
-                                        'max-w-xs',
-                                    ]) />
+                    <form action="{{ route('admin.categories.store') }}" method="POST" class="space-y-4 needs-validation"
+                        novalidate>
+                        @csrf
+                        <label class="form-control w-full">
+                            <div class="label">
+                                <span class="label-text">@lang('admin.post.title')</span>
                             </div>
-                            <div>
-                                <a href="{{ route('admin.categories.index') }}" class="btn-light btn">@lang('admin.btn.cancel')
-                                </a>
-                                <button type="submit" class="btn btn-success ml-2">
-                                    @lang('admin.btn.submit')
-                                </button>
-                            </div>
-                        </form>
-                    </div>
+                            <input type="text" name="title" placeholder="Type here"
+                                @class([
+                                    'input',
+                                    'input-bordered',
+                                    'input-error' => $errors->has('title'),
+                                    'w-full',
+                                ]) />
+                        </label>
+                        <div class="flex justify-end gap-4">
+                            <a href="{{ route('admin.categories.index') }}" class="btn-light btn">
+                                @lang('admin.btn.cancel')
+                            </a>
+                            <button type="submit" class="btn btn-success ml-2">
+                                @lang('admin.btn.submit')
+                            </button>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
