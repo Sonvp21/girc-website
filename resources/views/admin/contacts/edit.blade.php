f5021aa4332331b7b7966ab54a0557433ea0dfc4
ronniefaith <ronnie.paralejas@gmail.com>
2024-05-20 21:13:46 +0700

    Add modified tinymce for Contacts

--- resources/views/admin/contacts/edit.blade.php
+++ resources/views/admin/contacts/edit.blade.php
@@ -11,62 +11,61 @@
             <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg">
                 <div class="bg-white p-4 shadow sm:rounded-lg sm:p-8">
                     <form action="{{ route('admin.contacts.update', ['contact' => $contact->id]) }}" method="POST"
-                        class="needs-validation" novalidate>
+                        class="space-y-4 needs-validation" novalidate>
                         @csrf
                         @method('patch')
-                        <div class="space-y-4">
-                            <label class="form-control w-full">
-                                <div class="label">
-                                    <span class="label-text">@lang('admin.contacts.name')</span>
+                        <label class="form-control w-full">
+                            <div class="label">
+                                <span class="label-text">@lang('admin.contacts.name')</span>
+                            </div>
+                            <input type="text" name="name" value="{{ old('name', $contact->name) }}"
+                                placeholder="Put name" @class([
+                                    'input',
+                                    'input-bordered',
+                                    'input-error' => $errors->has('name'),
+                                    'w-full',
+                                ]) />
+                        </label>
+                        <label class="form-control w-full">
+                            <div class="label">
+                                <span class="label-text">@lang('admin.contacts.email')</span>
+                            </div>
+                            <input type="text" name="email" value="{{ old('email', $contact->email) }}"
+                                placeholder="email..." @class([
+                                    'input',
+                                    'input-bordered',
+                                    'input-error' => $errors->has('email'),
+                                    'w-full',
+                                ]) />
+                        </label>
+                        <label class="form-control w-full">
+                            <div class="label">
+                                <span class="label-text">@lang('admin.contacts.phone')</span>
+                            </div>
+                            <input type="text" name="phone" value="{{ old('phone', $contact->phone) }}"
+                                placeholder="0987...." @class([
+                                    'input',
+                                    'input-bordered',
+                                    'input-error' => $errors->has('phone'),
+                                    'w-full',
+                                ]) />
+                        </label>
+                        <label class="form-control w-full">
+                                <div class="label min-h">
+                                    <span class="label-text">@lang('admin.contacts.content')</span>
                                 </div>
-                                <input type="text" name="name" value="{{ old('name', $contact->name) }}"
-                                    placeholder="Put name" @class([
-                                        'input',
-                                        'input-bordered',
-                                        'input-error' => $errors->has('name'),
-                                        'w-full',
-                                    ]) />
+                                <textarea name="content" id="content" @class([
+                                    'w-full',
+                                    'input',
+                                    'input-bordered',
+                                    'input-error' => $errors->has('content'),
+                                    'min-h-72',
+                                ])> {!! old('content', $contact->content) !!}</textarea>
+                                @error('content')
+                                    <p class="mt-2 text-xs text-red-500">{{ $message }}</p>
+                                @enderror
                             </label>
-                            <label class="form-control w-full">
-                                <div class="label">
-                                    <span class="label-text">@lang('admin.contacts.email')</span>
-                                </div>
-                                <input type="text" name="email" value="{{ old('email', $contact->email) }}"
-                                    placeholder="email..." @class([
-                                        'input',
-                                        'input-bordered',
-                                        'input-error' => $errors->has('email'),
-                                        'w-full',
-                                    ]) />
-                            </label>
-                            <label class="form-control w-full">
-                                <div class="label">
-                                    <span class="label-text">@lang('admin.contacts.phone')</span>
-                                </div>
-                                <input type="text" name="phone" value="{{ old('phone', $contact->phone) }}"
-                                    placeholder="0987...." @class([
-                                        'input',
-                                        'input-bordered',
-                                        'input-error' => $errors->has('phone'),
-                                        'w-full',
-                                    ]) />
-                            </label>
-                            <label class="form-control w-full">
-                                    <div class="label min-h">
-                                        <span class="label-text">@lang('admin.contacts.content')</span>
-                                    </div>
-                                    <textarea name="content" id="content" @class([
-                                        'w-full',
-                                        'input',
-                                        'input-bordered',
-                                        'input-error' => $errors->has('content'),
-                                        'min-h-72',
-                                    ])> {!! old('content', $contact->content) !!}</textarea>
-                                    @error('content')
-                                        <p class="mt-2 text-xs text-red-500">{{ $message }}</p>
-                                    @enderror
-                                </label>
-                            <div class="flex justify-end gap-4">
+                        <div class="flex justify-end gap-4">
                                 <a href="{{ route('admin.contacts.index') }}" class="btn-light btn">
                                     @lang('admin.btn.cancel')
                                 </a>
@@ -74,7 +73,6 @@
                                     @lang('admin.btn.submit')
                                 </button>
                             </div>
-                        </div>
                     </form>
                 </div>
             </div>
