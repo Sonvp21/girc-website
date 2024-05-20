c6e752b89bb82c83a315a20844088f3b13b2675b
Son-vp-21 <nguyensonvp21@gmail.com>
2024-05-20 10:55:49 +0700

    done change form search and button add

--- resources/views/admin/albums/cooperations/index.blade.php
+++ resources/views/admin/albums/cooperations/index.blade.php
@@ -9,34 +9,28 @@
             <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg">
                 <div class="overflow-x-auto">
                     <div class="flex px-6 py-4">
-                        <form
-                            action="{{ route('admin.cooperations.index') }}"
-                            method="GET"
-                        >
-                            <div class="flex items-center">
-                                <input
-                                    type="text"
-                                    name="search"
-                                    placeholder="Search by title"
-                                    class="text-gray-800 border-gray-200 mr-0 rounded-l-lg border-b border-l border-t bg-white p-2"
-                                    value="{{ request()->search }}"
-                                />
-                                <button
-                                    type="submit"
-                                    class="bg-gray-200 text-gray-800 rounded-r-lg border-b border-r border-t p-2 px-4 font-semibold"
-                                >
-                                    Search
-                                </button>
+                        <form action="{{ route('admin.cooperations.index') }}" method="GET" class="w-full">
+                            <div class="flex items-center justify-between">
+                                <div class="flex items-center">
+                                    <label class="input input-bordered flex items-center gap-2">
+                                        <input name="search" type="text" class="grow" placeholder="Search by name"
+                                            style="border: unset" value="{{ request()->search }}" />
+                                        <button type="submit">
+                                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor"
+                                                class="h-4 w-4 opacity-70">
+                                                <path fill-rule="evenodd"
+                                                    d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
+                                                    clip-rule="evenodd" />
+                                            </svg>
+                                        </button>
+                                    </label>
+                                </div>
+                                <a class="btn-ghosdt btn" href="{{ route('admin.cooperations.create') }}">
+                                    <x-heroicon-s-plus class="size-4" />
+                                    <span>@lang('admin.add')</span>
+                                </a>
                             </div>
                         </form>
-                        <div class="ml-auto self-center">
-                            <a
-                                class="flex items-center justify-end"
-                                href="{{ route('admin.cooperations.create') }}"
-                                ><x-heroicon-o-plus-circle class="size-4" />
-                                @lang('admin.add')
-                            </a>
-                        </div>
                     </div>
 
                     <table class="table">
@@ -56,10 +50,8 @@
                                 <tr>
                                     <th>{{ $cooperations->firstItem() + $loop->index }}</th>
                                     <td>
-                                        <a
-                                            target="_blank"
-                                            href="{{ Str::startsWith($cooperation->link_website, ['http://', 'https://']) ? $cooperation->link_website : 'http://'.$cooperation->link_website }}"
-                                        >
+                                        <a target="_blank"
+                                            href="{{ Str::startsWith($cooperation->link_website, ['http://', 'https://']) ? $cooperation->link_website : 'http://' . $cooperation->link_website }}">
                                             {{ $cooperation->name }}
                                         </a>
                                     </td>
@@ -68,20 +60,14 @@
                                     <td>{{ $cooperation->updatedAtVi }}</td>
 
                                     <td class="flex gap-3">
-                                        <a href="{{ route('admin.cooperations.edit', $cooperation->id) }}"
-                                            ><x-heroicon-s-pencil-square class="size-4 text-green-600"
-                                        /></a>
-                                        <form
-                                            id="delete-form-{{ $cooperation->id }}"
+                                        <a href="{{ route('admin.cooperations.edit', $cooperation->id) }}"><x-heroicon-s-pencil-square
+                                                class="size-4 text-green-600" /></a>
+                                        <form id="delete-form-{{ $cooperation->id }}"
                                             action="{{ route('admin.cooperations.destroy', ['cooperation' => $cooperation->id]) }}"
-                                            method="POST"
-                                        >
+                                            method="POST">
                                             @csrf
                                             @method('DELETE')
-                                            <button
-                                                type="button"
-                                                onclick="confirmDelete({{ $cooperation->id }})"
-                                            >
+                                            <button type="button" onclick="confirmDelete({{ $cooperation->id }})">
                                                 <x-heroicon-o-trash class="size-4 text-red-500" />
                                             </button>
                                         </form>
