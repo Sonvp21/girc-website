511b60601039cf72dc28e21d75a19afe33b654eb
ronniefaith <ronnie.paralejas@gmail.com>
2024-05-24 11:21:08 +0700

    Fit footer and banner on mobile

--- resources/views/components/website/infrastructure.blade.php
+++ resources/views/components/website/infrastructure.blade.php
@@ -1,6 +1,6 @@
 <div class="col-span-8 space-y-3 md:col-span-6 lg:col-span-6">
     <x-website.partials.header main="true" title="{{ __('web.infrastructure') }}" textAlign="left" />
-    <ul class="flex gap-6">
+    <ul class="flex gap-6 w-ful overflow-x-auto">
         <li class="rounded-lg bg-gray-200 h-40 w-full flex items-center justify-center">
             <x-heroicon-s-play-circle class="size-24 text-white"/>
         </li>
