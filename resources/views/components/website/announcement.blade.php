bc1cedc5732d3f36eab9200e3a800ffd476029d0
ronniefaith <ronnie.paralejas@gmail.com>
2024-05-25 00:31:22 +0700

    quick corrections

--- resources/views/components/website/announcement.blade.php
+++ resources/views/components/website/announcement.blade.php
@@ -2,20 +2,22 @@
     <x-website.partials.header title="<a href='{{ route('announcements.index') }}' class='text-red-500'>{{ __('web.announcement_home') }}</a>" paddingTop="0" textColor="text-red-500" />
     <ul class="flex h-auto flex-col divide-y divide-solid divide-gray-300 overflow-scroll overflow-y-auto overscroll-contain border border-slate-200 bg-gray-100 px-4 text-sm scrollbar-hide">
         @foreach ($announcements as $announcement)
-            <li class="gap-2 py-4 text-xs">
-                <div class="gap float-left mr-2 divide-y divide-blue-200 overflow-hidden rounded-lg bg-[#fd9f1b] shadow-calendar">
-                    <div class="flex-none whitespace-nowrap py-0.5 text-center text-[0.6rem] font-bold text-white">
-                        <span class="px-2">{{ $announcement->published_at->format('n/Y') }}</span>
-                        <div class="w-full border-b border-dashed border-[#f37303]"></div>
+            <li>
+                <a href="{{ route('announcements.show', $announcement) }}" class="inline-block gap-2 py-4 text-xs text-gray-500 hover:text-blue-700">
+                    <div class="gap float-left mr-2 divide-y divide-blue-200 overflow-hidden rounded-lg bg-[#fd9f1b] shadow-calendar">
+                        <div class="flex-none whitespace-nowrap py-0.5 text-center text-[0.6rem] font-bold text-white">
+                            <span class="px-2">{{ $announcement->published_at->format('n/Y') }}</span>
+                            <div class="w-full border-b border-dashed border-[#f37303]"></div>
+                        </div>
+                        <div class="flex h-auto flex-col border-b-2 border-red-500 bg-white text-center text-slate-700">
+                            <span class="relative top-0.5 text-base font-bold leading-3">{{ $announcement->published_at->translatedFormat('d') }}</span>
+                            <span class="h-4 text-[0.5rem] font-bold capitalize text-[#fd9f1b]">{{ $announcement->published_at->translatedFormat('l') }}</span>
+                        </div>
                     </div>
-                    <div class="flex h-auto flex-col border-b-2 border-red-500 bg-white text-center text-slate-700">
-                        <span class="relative top-0.5 text-base font-bold leading-3">{{ $announcement->published_at->translatedFormat('d') }}</span>
-                        <span class="h-4 text-[0.5rem] font-bold capitalize text-[#fd9f1b]">{{ $announcement->published_at->translatedFormat('l') }}</span>
-                    </div>
-                </div>
-                <h3 class="line-clamp-3 h-12 text-justify font-normal leading-4 tracking-normal text-gray-500">
-                    {{ $announcement->title }}
-                </h3>
+                    <h3 class="line-clamp-3 h-12 text-justify font-normal leading-4 tracking-normal">
+                        {{ $announcement->title }}
+                    </h3>
+                </a>
             </li>
         @endforeach
     </ul>
--- resources/views/web/announcements/index.blade.php
+++ resources/views/web/announcements/index.blade.php
@@ -8,28 +8,30 @@
                             @lang('web.announcements')
                         </h2>
                     </div>
-                    <ul class="mt-5 ">
+                    <ul class="mt-5">
                         @foreach ($announcements as $announcement)
-                            <li class="flex border-b border-yellow-500 border-dashed py-4">
+                            <li class="flex border-b border-dashed border-yellow-500 py-4">
                                 <article class="group w-full">
-                                    <div class="flex gap-3 ">
-                                        <div class="gap float-left mr-2 divide-y divide-blue-200 bg-yellow-400 text-red-500">
-                                            <div class="h-4 w-12 whitespace-nowrap text-center text-[10px]">
-                                                @lang('web.month')
-                                                {{ $announcement->published_at->translatedFormat('m') }}
+                                    <div class="flex gap-3">
+                                        <div class="gap float-left mr-2 divide-y divide-blue-200 overflow-hidden rounded-lg bg-[#fd9f1b] shadow-calendar">
+                                            <div class="flex-none whitespace-nowrap py-0.5 text-center text-[0.6rem] font-bold text-white">
+                                                <span class="px-2">{{ $announcement->published_at->format('n/Y') }}</span>
+                                                <div class="w-full border-b border-dashed border-[#f37303]"></div>
                                             </div>
-                                            <div class="text h-8 w-12 text-center text-xl font-extrabold">{{ $announcement->published_at->translatedFormat('d') }}</div>
+                                            <div class="flex h-auto flex-col border-b-2 border-red-500 bg-white text-center text-slate-700">
+                                                <span class="relative top-0.5 text-base font-bold leading-3">{{ $announcement->published_at->translatedFormat('d') }}</span>
+                                                <span class="h-4 text-[0.5rem] font-bold capitalize text-[#fd9f1b]">{{ $announcement->published_at->translatedFormat('l') }}</span>
+                                            </div>
                                         </div>
-                                        <h3 class="line-clamp-3 h-12 font-normal leading-4 tracking-normal text-blue-950 hover:text-red-500 text-justify">
+                                        <h3 class="line-clamp-3 h-12 text-justify font-normal leading-4 tracking-normal text-blue-950 hover:text-red-500">
                                             <a href="{{ route('announcements.show', $announcement) }}">{{ $announcement->title }}</a>
                                         </h3>
-                                        
                                     </div>
                                 </article>
                             </li>
                         @endforeach
                     </ul>
-                    
+
                     {{ $announcements->links('pagination.web-tailwind') }}
                 </div>
                 <div class="col-span-8 hidden space-y-3 md:col-span-2 lg:block">
