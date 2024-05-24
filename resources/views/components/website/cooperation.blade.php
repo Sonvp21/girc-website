c6b4d25b1d0814718dea6057a1dd7ecbdb7e6878
ronniefaith <ronnie.paralejas@gmail.com>
2024-05-24 17:30:31 +0700

    Add real data

--- resources/views/components/website/cooperation.blade.php
+++ resources/views/components/website/cooperation.blade.php
@@ -1,13 +1,22 @@
 <div class="col-span-8 space-y-3 md:col-span-6 lg:col-span-6">
     <x-website.partials.header main="true" title="{{ __('web.cooperation') }}" textAlign="left" />
-    <div class="main-carousel border-b-8 border-blue-500 bg-gray-200 p-5" data-flickity='{ "autoPlay": true, "wrapAround": true, "pageDots": false, "isWrapped": true }'>
-        @for ($i = 0; $i < 14; $i++)
-            <div class="carousel-cell">
-                <div class="mx-2 flex h-24 w-36 items-center justify-center rounded-lg border border-gray-300 bg-white p-4">
-                    <x-heroicon-o-photo class="size-14 text-blue-100" />
+    @unless ($cooperations->isEmpty())
+        <div class="main-carousel h-36 overflow-hidden border-b-8 border-blue-500 bg-gray-200 p-5" data-flickity='{ 
+                "autoPlay": true, 
+                "pageDots": false, 
+                "wrapAround": true, 
+                "contain": true, 
+                "cellAlign": "left",
+                "index": 0
+            }'>
+            @foreach ($cooperations as $cooperation)
+                <div class="carousel-cell">
+                    <a href="{{ $cooperation->link_website }}" target="_blank" class="mx-2 flex h-24 w-36 items-center justify-center overflow-hidden rounded-lg border border-gray-300 bg-white">
+                        <img src="{{ $cooperation->getFirstMedia('album_cooperation')->getUrl('lg') }}" alt="{{ $cooperation->name }}">
+                    </a>
                 </div>
-            </div>
-        @endfor
-        </ul>
-    </div>
+            @endforeach
+        </div>
+    @endunless
+
 </div>
