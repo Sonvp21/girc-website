df34232f6fb6d2c7c0057d8bf84d857d756e2b7a
ronniefaith <ronnie.paralejas@gmail.com>
2024-05-24 16:23:02 +0700

    Apply flickity

--- resources/views/components/website/cooperation.blade.php
+++ resources/views/components/website/cooperation.blade.php
@@ -1,12 +1,13 @@
 <div class="col-span-8 space-y-3 md:col-span-6 lg:col-span-6">
     <x-website.partials.header main="true" title="{{ __('web.cooperation') }}" textAlign="left" />
-    <ul class="flex flex-nowrap gap-2 py-5 bg-gray-200 border-b-8 border-blue-500 px-6 overflow-x-auto">
-        @for($i = 0; $i < 14; $i++)
-            <li class="rounded-lg flex items-center justify-center">
-                <div class="p-4 rounded-lg bg-white border border-gray-300 h-24 w-36 flex items-center justify-center">
-                    <x-heroicon-o-photo class="size-14 text-blue-100"/>
+    <div class="main-carousel border-b-8 border-blue-500 bg-gray-200 p-5" data-flickity='{ "autoPlay": true, "wrapAround": true, "pageDots": false, "isWrapped": true }'>
+        @for ($i = 0; $i < 14; $i++)
+            <div class="carousel-cell">
+                <div class="mx-2 flex h-24 w-36 items-center justify-center rounded-lg border border-gray-300 bg-white p-4">
+                    <x-heroicon-o-photo class="size-14 text-blue-100" />
                 </div>
-            </li>
+            </div>
         @endfor
-    </ul>
+        </ul>
+    </div>
 </div>
