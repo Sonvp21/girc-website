73a795f38cbd11b9390d347b7ad0e395dc7ef3bd
ronniefaith <ronnie.paralejas@gmail.com>
2024-05-25 16:56:33 +0700

    Minor refactoring

--- app/View/Components/Website/HomePost.php
+++ app/View/Components/Website/HomePost.php
@@ -10,22 +10,19 @@ use Illuminate\View\Component;
 class HomePost extends Component
 {
     public function render(): View|Closure|string
-{
-    $categoryId = config('app.home_category_id');
+    {
+        $posts = Post::query()
+            ->with('category')
+            ->whereHas('category', function ($query) {
+                $query->whereId(config('app.home_category_id'));
+            })
+            ->latest('published_at')
+            ->take(5)
+            ->get();
 
-    $posts = Post::query()
-        ->with('category')
-        ->whereHas('category', function ($query) use ($categoryId) {
-            $query->whereId($categoryId);
-        })
-        ->latest('published_at')
-        ->take(5)
-        ->get();
-
-    return view('components.website.home-post', [
-        'posts' => $posts,
-        'latestPost' => $posts->first(),
-    ]);
-}
-
+        return view('components.website.home-post', [
+            'posts' => $posts,
+            'latestPost' => $posts->first(),
+        ]);
+    }
 }
