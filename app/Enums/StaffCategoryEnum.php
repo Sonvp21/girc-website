2dc834487cbb928ddad514a8dac560630b9a2307
ronniefaith <ronnie.paralejas@gmail.com>
2024-05-25 10:50:48 +0700

    introduce enum

---
+++ app/Enums/StaffCategoryEnum.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Enums;
+
+enum StaffCategoryEnum: string
+{
+    case BASIC_SCIENCE = 'basic_science';
+    case INFORMATION_TECHNOLOGY = 'information_technology';
+    case ECONOMY_AND_CREATED_NEWS = 'economy_and_created_news';
+    case INTER_INDUSTRY_APPLICATION = 'inter_industry_application';
+}
