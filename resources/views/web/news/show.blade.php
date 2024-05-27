72edc6eaf8799f78033f951a5840ee748892c8a5
Son-vp-21 <nguyensonvp21@gmail.com>
2024-05-27 07:50:24 +0700

    next

--- resources/views/web/news/show.blade.php
+++ resources/views/web/news/show.blade.php
@@ -2,23 +2,38 @@
     <section>
         <div class="mx-auto max-w-7xl px-3 sm:px-6 md:items-center lg:px-8">
             <div class="grid grid-cols-8 gap-4">
-                <div class="col-span-8 space-y-3 md:col-span-6 lg:col-span-6">
-                    <div class="border-b-2 border-blue-700">
-                        <h2 class="inline-block bg-blue-700 px-6 py-3 text-xl font-bold text-white">@lang('web.news_events')</h2>
-                    </div>
-                    <div class="breadcrumbs w-full text-sm">
-                        <ul>
-                            <li>
-                                <a href="{{ route('news.index') }}">@lang('web.news')</a>
-                            </li>
-                            <li>{{ $post->category->title }}</li>
-                        </ul>
+                <div class="col-span-8 md:col-span-6 lg:col-span-6">
+                    <div>
+                        <div class="text-sm breadcrumbs p-4 text-blue-800">
+                            <ul>
+                                <li>
+                                    <a href="{{ route('home') }}">
+                                        <x-heroicon-o-home class="size-5" />
+                                        Home
+                                    </a>
+                                </li>
+
+                                <li>
+                                    <a><x-heroicon-o-folder
+                                            class="size-5" />
+                                        {{ app()->getLocale() === 'en' ? $post->category->title_en : $post->category->title }}
+                                    </a>
+                                </li>
+                                <li>
+                                    <a
+                                        href="{{ route('news.show', $post) }}">
+                                        <x-heroicon-o-folder-open class="size-5" />
+                                        {{ Str::limit($post->title, 100) }}
+                                    </a>
+                                </li>
+
+                            </ul>
+                        </div>
+                        <div class="bg-gradient-to-r from-blue-400 via-blue-500 via-70% to-red-500 h-0.5"></div>
                     </div>
                     <article class="group">
-                        <div
-                            class="tooltip tooltip-top flex items-center gap-2 text-green-700"
-                            data-tip="{{ $post->published_post_date }}"
-                        >
+                        <div class="tooltip tooltip-top flex items-center gap-2 text-green-700"
+                            data-tip="{{ $post->published_post_date }}">
                             <x-heroicon-m-calendar class="size-4" />
                             <span class="text-xs">{{ $post->published_post_date_thumb }}</span>
                         </div>
@@ -35,11 +50,7 @@
                         <ul class="flex items-center gap-2">
                             @foreach ($post->tags as $tag)
                                 <li>
-                                    <a
-                                        href="#"
-                                        class="btn btn-xs"
-                                        >{{ $tag->name }}</a
-                                    >
+                                    <a href="#" class="btn btn-xs">{{ $tag->name }}</a>
                                 </li>
                             @endforeach
                         </ul>
