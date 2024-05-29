df42450629632ac2607c0d54ee3d9d7e9857a650
Son-vp-21 <nguyensonvp21@gmail.com>
2024-05-29 08:01:56 +0700

    highlight selected

--- resources/views/components/website/dynamic-menu.blade.php
+++ resources/views/components/website/dynamic-menu.blade.php
@@ -1,35 +1,20 @@
-@php
-    if (!function_exists('isCategorySelected')) {
-        function isCategorySelected($category, $slug): bool
-        {
-            if ($category->slug == $slug) {
-                return true;
-            }
-
-            foreach ($category->children as $child) {
-                if (isCategorySelected($child, $slug)) {
-                    return true;
-                }
-            }
-
-            return false;
-        }
-    }
-    
-@endphp
-
 @if ($category->children->isNotEmpty())
-    <li class="relative flex-row whitespace-nowrap" x-data="{ dropdownOpen: false }" @mouseover.away="dropdownOpen = false">
-        <div class="flex hover:bg-blue-800 hover:text-white" :class="{ 'bg-black bg-opacity-20': dropdownOpen }">
-            <button class="flex h-full w-full flex-row items-center justify-between gap-2 py-4 text-center font-semibold uppercase tracking-wider text-white focus:outline-none" @mouseover="dropdownOpen = true">
-                <span class="lg:border-r flex w-full justify-between gap-2 border-white px-2 lg:w-auto lg:justify-start">
-                   {{ $category->title }}
+    <li class="relative flex-row whitespace-nowrap {{ request()->is('categories/' . $category->slug) || request()->is('categories/' . $category->slug . '/*') ? 'active' : '' }}"
+        x-data="{ dropdownOpen: false }" @mouseover.away="dropdownOpen = false">
+        <div class="flex {{ request()->is('categories/' . $category->slug) || request()->is('categories/' . $category->slug . '/*') ? 'bg-blue-600 text-white' : 'hover:bg-blue-600 hover:text-white' }}" :class="{ 'bg-black bg-opacity-20': dropdownOpen }">
+            <button
+                class="flex h-full w-full flex-row items-center justify-between gap-2 py-4 text-center font-semibold uppercase tracking-wider text-white focus:outline-none"
+                @mouseover="dropdownOpen = true">
+                <span class="flex w-full justify-between gap-2 border-white px-2 lg:w-auto lg:justify-start {{ $isChild ? '' : 'lg:border-r' }}">
+                    {{ $category->title }}
                     <x-heroicon-c-chevron-down class="size-4" />
                 </span>
             </button>
         </div>
-        <div class="hidden origin-top-left bg-white shadow-md md:w-auto lg:absolute" :class="{ 'block': dropdownOpen, 'hidden': !dropdownOpen }" style="width: -webkit-fill-available;">
-            <ul class="text-denim-600 divide-denim-400 w-full origin-top-left divide-y divide-dashed whitespace-nowrap bg-white">
+        <div class="hidden origin-top-left bg-gray-300 shadow-md md:w-auto lg:absolute"
+            :class="{ 'block': dropdownOpen, 'hidden': !dropdownOpen }">
+            <ul
+                class="text-black divide-gray-400 origin-top-left divide-y divide-dashed whitespace-nowrap bg-white w-auto">
                 @foreach ($category->children as $child)
                     <x-website.dynamic-menu :category="$child" isChild="true" />
                 @endforeach
@@ -37,20 +22,11 @@
         </div>
     </li>
 @else
-    @if($isChild)
-        <li class="hover:bg-black hover:bg-opacity-5">
-            <a href="{{ $link }}" class="flex items-center justify-start space-x-2 px-2 py-4 text-slate-800">
-                {{ $category->title }}
-            </a>
-        </li>
-    @else
-        <li>
-            <a href="{{ $link }}" class="flex h-full items-center justify-start py-4 font-semibold uppercase tracking-wider text-white hover:bg-blue-800 hover:text-white">
-                <span class="lg:border-r border-white px-2">{{ $category->title }}</span>
-            </a>
-        </li>
-    @endif
+    <li class="{{ request()->is('categories/' . $category->slug) || request()->is('categories/' . $category->slug . '/*') ? ($isChild ? 'active bg-gray-300 text-black' : 'active bg-blue-600 text-white') : ($isChild ? 'hover:bg-gray-300 hover:text-black' : 'hover:bg-blue-600 hover:text-white') }}">
+        <a href="{{ $link }}"
+            class="flex h-full items-center justify-start py-4 font-semibold uppercase tracking-wider {{ $isChild ? 'text-black' : 'text-white' }}" style="background-color: inherit;">
+            <span class="{{ $isChild ? 'text-black normal-case font-sans' : 'lg:border-r text-white font-roboto' }} border-white px-2">{{ $category->title }}</span>
+        </a>
+    </li>
 @endif
-
-
 
--- resources/views/components/website/menu.blade.php
+++ resources/views/components/website/menu.blade.php
@@ -1,19 +1,22 @@
 <nav class="sticky top-0 z-50 w-full bg-blue-700 shadow" x-data="{ navOpen: false }">
     <div class="lg:hidden">
-        <button @click="navOpen = !navOpen" class="hover:bg-denim-600 border-denim-600 focus:shadow-outline flex h-full items-center px-3 py-4 text-white focus:outline-none lg:border-l">
+        <button @click="navOpen = !navOpen"
+            class="hover:bg-denim-600 border-denim-600 focus:shadow-outline flex h-full items-center px-3 py-4 text-white focus:outline-none lg:border-l">
             <x-heroicon-o-bars-3-bottom-left class="size-5" />
         </button>
     </div>
-    <div :class="{ 'flex': navOpen, 'hidden': !navOpen }" class="mx-auto hidden max-w-7xl flex-col justify-between px-3 sm:px-6 md:items-center lg:flex lg:flex-row lg:px-8">
+    <div :class="{ 'flex': navOpen, 'hidden': !navOpen }"
+        class="mx-auto hidden max-w-7xl flex-col justify-between px-3 sm:px-6 md:items-center lg:flex lg:flex-row lg:px-8">
         <ul class="text-normal flex w-full flex-col text-xs lg:flex-row lg:divide-x-0 lg:divide-y-0 divide-dashed">
-            <li>
-                <a href="/" class="flex h-full w-full flex-row items-center justify-start gap-2 py-2 text-center font-semibold uppercase tracking-wider text-white hover:bg-blue-800 focus:outline-none">
+            <li class="{{ request()->routeIs('home') ? 'active' : '' }}">
+                <a href="{{ route('home') }}"
+                    class="flex h-full w-full flex-row items-center justify-start gap-2 py-2 text-center font-semibold uppercase tracking-wider text-white hover:bg-blue-600 focus:outline-none {{ request()->routeIs('home') ? 'bg-blue-600' : '' }}">
                     <span class="lg:border-r border-white px-2">
                         <x-heroicon-o-home class="size-5" />
                     </span>
                 </a>
             </li>
-            @foreach($categories as $category)
+            @foreach ($categories as $category)
                 <li class="relative flex-row whitespace-nowrap">
                     <x-website.dynamic-menu :category="$category" />
                 </li>
