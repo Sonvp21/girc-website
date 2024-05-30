eb067ffbcff4bb684c48097fd0e8451aac59043c
Son-vp-21 <nguyensonvp21@gmail.com>
2024-05-30 15:33:16 +0700

    fix show menu child

--- resources/views/components/website/dynamic-menu.blade.php
+++ resources/views/components/website/dynamic-menu.blade.php
@@ -14,7 +14,7 @@
         <div class="hidden origin-top-left bg-gray-200 shadow-md md:w-auto lg:absolute"
             :class="{ 'block': dropdownOpen, 'hidden': !dropdownOpen }">
             <ul
-                class="text-black divide-gray-200 origin-top-left divide-y divide-dashed whitespace-nowrap bg-white w-auto">
+                class="text-black divide-gray-100 origin-top-left divide-y divide-dashed whitespace-nowrap bg-white w-auto">
                 @foreach ($category->children as $child)
                     <x-website.dynamic-menu :category="$child" isChild="true" />
                 @endforeach
@@ -22,10 +22,13 @@
         </div>
     </li>
 @else
-    <li class="{{ request()->is('categories/' . $category->slug) || request()->is('categories/' . $category->slug . '/*') ? ($isChild ? 'active bg-gray-100 text-black' : 'active bg-blue-600 text-white') : ($isChild ? 'hover:bg-gray-200 hover:text-black' : 'hover:bg-blue-600 hover:text-white') }}">
+    <li class="{{ request()->is('categories/' . $category->slug) || request()->is('categories/' . $category->slug . '/*') ? ($isChild ? 'active bg-gray-100 text-black' : 'active bg-blue-600 text-white') : ($isChild ? 'hover:bg-gray-100 hover:text-black h-9' : 'hover:bg-blue-600 hover:text-white') }}">
         <a href="{{ $link }}"
-            class="flex h-full items-center justify-start py-4 font-semibold uppercase tracking-wider {{ $isChild ? 'text-black' : 'text-white' }}" style="background-color: inherit;">
-            <span class="{{ $isChild ? 'text-black normal-case font-sans' : 'lg:border-r text-white font-roboto' }} border-white px-2">{{ $category->title }}</span>
+            class="flex h-full items-center justify-start py-4 uppercase tracking-wider {{ $isChild ? 'text-black font-sans' : 'text-white font-semibold' }}" style="background-color: inherit;">
+            @if ($isChild)
+            <x-heroicon-o-chevron-right class="size-3 text-gray-600 ml-2"/>
+            @endif
+            <span class="{{ $isChild ? 'text-gray-600 normal-case font-sans' : 'lg:border-r text-white font-roboto' }} border-white px-2">{{ $category->title }}</span>
         </a>
     </li>
 @endif
