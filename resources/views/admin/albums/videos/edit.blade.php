3156833bea4eda0df5a6d21a1084c429989a2ffb
Son-vp-21 <nguyensonvp21@gmail.com>
2024-05-30 10:55:58 +0700

    fix show img edit videos

--- resources/views/admin/albums/videos/edit.blade.php
+++ resources/views/admin/albums/videos/edit.blade.php
@@ -110,4 +110,24 @@
         </div>
     </div>
     </div>
+    @pushonce('bottom_scripts')
+        <x-admin.forms.tinymce-config column="content" model="Photo"/>
+        <script>
+            var loadFile = function(event) {
+                var input = event.target
+                var file = input.files[0]
+                var type = file.type
+
+                var output = document.getElementById('preview_img')
+                var fileNameSpan = document.getElementById('selected_file_name')
+
+                output.src = URL.createObjectURL(event.target.files[0])
+                output.onload = function() {
+                    URL.revokeObjectURL(output.src) // free memory
+                }
+
+                fileNameSpan.innerText = file.name
+            }
+        </script>
+    @endpushonce
 </x-app-layout>
