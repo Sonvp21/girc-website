84107ab9c7b9ddd819fcd5bebe0fcf7c8c0b5ad1
Son-vp-21 <nguyensonvp21@gmail.com>
2024-06-01 21:45:03 +0700

    add title header

--- resources/views/components/website/staff.blade.php
+++ resources/views/components/website/staff.blade.php
@@ -12,19 +12,24 @@
     @foreach ($categories as $index => $category)
     <dialog id="staff-modal-{{ $index }}" class="modal">
         <div class="modal-box relative sm:min-h-fit md:h-[inherit] p-2 min-w-[60%] md:min-h-[90%]">
-            <div class="mt-[30px] border border-black rounded-lg mx-auto max-w-7xl sm:px-2 h-full sm:h-[90%]">
+            <div class="bg-blue-800 inline-block relative py-2 px-4">
+                <svg fill="currentColor" class="absolute -right-[3rem] top-0 z-0 h-10 text-blue-700 transform scale-y-[-1]"
+                    xmlns="http://www.w3.org/2000/svg" shape-rendering="geometricPrecision" text-rendering="geometricPrecision"
+                    viewBox="0 0 120 36">
+                    <path d="M77.103718 36C98.551859 36 98.551859 0 120 0H42.896282C21.448141 0 21.448141 36 0 36h77.103718Z">
+                    </path>
+                </svg>
+                <h3 class="relative z-20 text-white uppercase whitespace-nowrap">@lang('web.teaching_staff')</h3>
+            </div>
+            <div class="border border-black rounded-lg rounded-l-none mx-auto max-w-7xl sm:px-2 h-full sm:h-[90%]">
                 <div class="py-1 h-full">
                     <h3 class="font-bold text-lg text-center font-times tracking-wide relative flex items-center justify-center">
                         <div class="flex-grow mx-2 bg-gradient-to-r from-blue-400 via-blue-500 via-50% to-red-500 h-0.5"></div>
                         <span class="px-2 font-serif italic">@lang('web.info_teaching')</span>
                         <div class="flex-grow mx-2 bg-gradient-to-r from-red-500 via-blue-500 via-50% to-blue-400 h-0.5"></div>
                     </h3>
-                    <div style="text-align: -webkit-center;">
-                        <div class="flex-grow mx-2 bg-gradient-to-r bg-gray-500 h-0.5 w-[18%]"></div>
-                        <div class="mt-2 flex-grow mx-2 bg-gradient-to-r bg-gray-500 via-50% h-0.5 w-[8%]"></div>
-                    </div>
     
-                    <div class="row-span-1 h-full">
+                    <div class="mt-3 row-span-1 h-full">
                         <div class="max-w-full overflow-auto h-[60%] content-container">
                             @php
                                 $latestStaff = $staffs
@@ -40,7 +45,7 @@
                         </div>
                         <h3 class="font-bold text-lg text-center font-times tracking-wide relative flex items-center justify-center">
                             <div class="flex-grow mx-2 bg-gradient-to-r from-blue-400 via-blue-500 via-50% to-red-500 h-0.5"></div>
-                            <span class="px-2 font-serif italic">Giảng viên {{ __('admin.' . strtolower($category->name)) }}</span>
+                            <span class="px-2 font-serif italic text-red-500">Giảng viên {{ __('admin.' . strtolower($category->name)) }}</span>
                             <div class="flex-grow mx-2 bg-gradient-to-r from-red-500 via-blue-500 via-50% to-blue-400 h-0.5"></div>
                         </h3>
                         <div class="main-carousel h-36 overflow-hidden mt-[1%]"
